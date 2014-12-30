fe047f69337547be743971521e94e5d7ec51b959
Joël Troch <JoelTroch@users.noreply.github.com>
2014-12-30 14:46:38 +0100

    Same fix applied to Ricochet

--- ricochet/dlls/weapons.cpp
+++ ricochet/dlls/weapons.cpp
@@ -1003,7 +1003,7 @@ void CBasePlayerAmmo :: DefaultTouch( CBaseEntity *pOther )
 //=========================================================
 int CBasePlayerWeapon::ExtractAmmo( CBasePlayerWeapon *pWeapon )
 {
-	int			iReturn;
+	int			iReturn = 0;
 
 	if ( pszAmmo1() != NULL )
 	{
