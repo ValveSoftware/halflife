08de54961d95c9dcb0c499945b082076951c79f8
Alibek Omarov <a1ba.omarov@gmail.com>
2015-04-04 22:30:25 +0600

    Android NDK makefiles

---
+++ dlls/Android.mk
@@ -0,0 +1,131 @@
+#HLSDK server Android port
+#Copyright (c) nicknekit
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := server
+
+LOCAL_CFLAGS += -D_LINUX -DCLIENT_WEAPONS -D_DEBUG \
+	-Dstricmp=strcasecmp -Dstrnicmp=strncasecmp -D_snprintf=snprintf \
+	-msoft-float -fno-exceptions -fsigned-char
+
+LOCAL_CPPFLAGS := $(LOCAL_CFLAGS) -frtti
+
+LOCAL_C_INCLUDES := $(SDL_PATH)/include \
+		    $(LOCAL_PATH)/. \
+		    $(LOCAL_PATH)/wpn_shared \
+		    $(LOCAL_PATH)/../common \
+		    $(LOCAL_PATH)/../engine/common \
+		    $(LOCAL_PATH)/../engine \
+		    $(LOCAL_PATH)/../public \
+		    $(LOCAL_PATH)/../pm_shared \
+		    $(LOCAL_PATH)/../game_shared
+
+LOCAL_SRC_FILES := aflock.cpp \
+           agrunt.cpp \
+           airtank.cpp \
+           animating.cpp \
+           animation.cpp \
+           apache.cpp \
+           barnacle.cpp \
+           barney.cpp \
+           bigmomma.cpp \
+           bloater.cpp \
+           bmodels.cpp \
+           bullsquid.cpp \
+           buttons.cpp \
+           cbase.cpp \
+           client.cpp \
+           combat.cpp \
+           controller.cpp \
+           crossbow.cpp \
+           crowbar.cpp \
+           defaultai.cpp \
+           doors.cpp \
+           effects.cpp \
+           egon.cpp \
+           explode.cpp \
+           flyingmonster.cpp \
+           func_break.cpp \
+           func_tank.cpp \
+           game.cpp \
+           gamerules.cpp \
+           gargantua.cpp \
+           gauss.cpp \
+           genericmonster.cpp \
+           ggrenade.cpp \
+           globals.cpp \
+           gman.cpp \
+           h_ai.cpp \
+           h_battery.cpp \
+           h_cine.cpp \
+           h_cycler.cpp \
+           h_export.cpp \
+           handgrenade.cpp \
+           hassassin.cpp \
+           headcrab.cpp \
+           healthkit.cpp \
+           hgrunt.cpp \
+           wpn_shared/hl_wpn_glock.cpp \
+           hornet.cpp \
+           hornetgun.cpp \
+           houndeye.cpp \
+           ichthyosaur.cpp \
+           islave.cpp \
+           items.cpp \
+           leech.cpp \
+           lights.cpp \
+           maprules.cpp \
+           monstermaker.cpp \
+           monsters.cpp \
+           monsterstate.cpp \
+           mortar.cpp \
+           mp5.cpp \
+           multiplay_gamerules.cpp \
+           nihilanth.cpp \
+           nodes.cpp \
+           osprey.cpp \
+	   observer.cpp \
+           pathcorner.cpp \
+           plane.cpp \
+           plats.cpp \
+           player.cpp \
+           python.cpp \
+           rat.cpp \
+           roach.cpp \
+           rpg.cpp \
+           satchel.cpp \
+           schedule.cpp \
+           scientist.cpp \
+           scripted.cpp \
+           shotgun.cpp \
+           singleplay_gamerules.cpp \
+           skill.cpp \
+           sound.cpp \
+           soundent.cpp \
+           spectator.cpp \
+           squadmonster.cpp \
+           squeakgrenade.cpp \
+           subs.cpp \
+           talkmonster.cpp \
+           teamplay_gamerules.cpp \
+           tempmonster.cpp \
+           tentacle.cpp \
+           triggers.cpp \
+           tripmine.cpp \
+           turret.cpp \
+           util.cpp \
+           weapons.cpp \
+           world.cpp \
+           xen.cpp \
+           zombie.cpp \
+	   ../pm_shared/pm_debug.c \
+	   ../pm_shared/pm_math.c \
+	   ../pm_shared/pm_shared.c \
+	   ../game_shared/voice_gamemgr.cpp
+
+LOCAL_LDLIBS := -llog
+
+include $(BUILD_SHARED_LIBRARY)
