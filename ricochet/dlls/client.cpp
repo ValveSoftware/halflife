630ba755a3a51bfdb5f3c0243abfecaa5f4869b8
Sam <duo929@hotmail.com>
2016-06-30 17:15:23 +0200

    Fix GetHullBounds & HUD_GetHullBounds assignment
    Fixed GetHullBounds and HUD_GetHullBounds assigning bounds incorrectly.
    Assigning vectors to float pointers does nothing useful.
    I didn't test the DMC and Ricochet changes, but they're identical to the
    Half-Life versions, so they should be fine.
    Resolves #1703

--- ricochet/dlls/client.cpp
+++ ricochet/dlls/client.cpp
@@ -1703,21 +1703,24 @@ int GetHullBounds( int hullnumber, float *mins, float *maxs )
 {
 	int iret = 0;
 
+	Vector& vecMins = *reinterpret_cast<Vector*>( mins );
+	Vector& vecMaxs = *reinterpret_cast<Vector*>( maxs );
+
 	switch ( hullnumber )
 	{
 	case 0:				// Normal player
-		mins = VEC_HULL_MIN;
-		maxs = VEC_HULL_MAX;
+		vecMins = VEC_HULL_MIN;
+		vecMaxs = VEC_HULL_MAX;
 		iret = 1;
 		break;
 	case 1:				// Crouched player
-		mins = VEC_DUCK_HULL_MIN;
-		maxs = VEC_DUCK_HULL_MAX;
+		vecMins = VEC_DUCK_HULL_MIN;
+		vecMaxs = VEC_DUCK_HULL_MAX;
 		iret = 1;
 		break;
 	case 2:				// Point based hull
-		mins = Vector( 0, 0, 0 );
-		maxs = Vector( 0, 0, 0 );
+		vecMins = Vector( 0, 0, 0 );
+		vecMaxs = Vector( 0, 0, 0 );
 		iret = 1;
 		break;
 	}
