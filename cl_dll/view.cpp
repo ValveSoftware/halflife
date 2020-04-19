fd380fc98ed5f3e74d464327c323e5ddc4b2a1ac
Sabian Roberts <31491602+barney-heatwave@users.noreply.github.com>
2020-04-19 17:24:23 +0100

    Fix #1544 — Part 4

--- cl_dll/view.cpp
+++ cl_dll/view.cpp
@@ -662,10 +662,11 @@ void V_CalcNormalRefdef ( struct ref_params_s *pparams )
 	view->origin[2] += bob;
 
 	// throw in a little tilt.
+	extern cvar_t *cl_bobtilt;
 	view->angles[YAW]   -= bob * 0.5;
 	view->angles[ROLL]  -= bob * 1;
 	view->angles[PITCH] -= bob * 0.3;
-
+	if (cl_bobtilt->value == 1) VectorCopy(view->angles, view->curstate.angles);
 	// pushing the view origin down off of the same X/Z plane as the ent's origin will give the
 	// gun a very nice 'shifting' effect when the player looks up/down. If there is a problem
 	// with view model distortion, this may be a cause. (SJB). 
